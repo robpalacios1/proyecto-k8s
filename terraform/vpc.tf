c39648545568889c4225b21ccbe951985d482b2c
roberto <robpalacios1@hotmail.com>
2026-04-06 09:45:11 -0500

    fix variables into vpc.tf file

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -49,14 +49,14 @@ resource "aws_subnet" "public_subnet_2" {
 # 4. Route Table
 
 resource "aws_route_table" "public_rt" {
-    vpc_id = aws_vpc.main_vpc_id
+    vpc_id = aws_vpc.main_vpc.id
 
     route {
         cidr_block = "0.0.0.0/0"
         gateway_id = aws_internet_gateway.igw.id
     }
 
-    tags {
+    tags = {
         Name = "api-python-public_rt"
     }
 }
