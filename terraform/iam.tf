4c78d3f9cb15917aeb86bae8a8916860613fb86f
roberto <robpalacios1@hotmail.com>
2026-04-06 15:22:35 -0500

    create roles for nodes in iam.tf file

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -23,3 +23,35 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
     role = aws_iam_role.eks_cluster_role.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+
+# 3. Roles for Nodes (Servers)
+
+resource "aws_iam_role" "eks_node_role" {
+    name = "api-python-eks-node-role"
+
+    assume_role_policy = jsonencode ({
+        Version = "2012-10-17"
+        Statement = [{
+            Action = "sts:AssumeRole"
+            Effect = "Allow"
+            Principal = {
+                Service = ec2.amazonaws.com
+            }
+        }]
+    })
+}
+
+resource "aws_iam_role_policy_attachment" "eks_node_policy" {
+    role = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+    role = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
+
+resource "aws_iam_role_policy_attachment" "eks_registry_policy" {
+    role = aws_iam_role.eks_node_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
