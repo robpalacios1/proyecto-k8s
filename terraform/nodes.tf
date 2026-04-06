6ea0e74e9f8f49e0b5aa7470802f03e06f220cc9
roberto <robpalacios1@hotmail.com>
2026-04-06 17:24:10 -0500

    change instance type to t3.micro

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -16,8 +16,8 @@ resource "aws_eks_node_group" "main_nodes" {
         min_size     = 1
     }
 
-    # Kind of instances (2 vCPU, 4GB RAM)
-    instance_types = ["t3.medium"]
+    # Kind of instances (2 vCPU, 1GB RAM)
+    instance_types = ["t3.micro"]
 
     depends_on  = [
         aws_iam_role_policy_attachment.eks_node_policy,
