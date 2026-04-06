0a95e751ebd8171e22e91ae147b3494d24958e00
roberto <robpalacios1@hotmail.com>
2026-04-06 16:02:59 -0500

    indentation json for iam.tf file

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -42,16 +42,16 @@ resource "aws_iam_role" "eks_node_role" {
 }
 
 resource "aws_iam_role_policy_attachment" "eks_node_policy" {
-    role = aws_iam_role.eks_node_role.name
+    role       = aws_iam_role.eks_node_role.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
-    role = aws_iam_role.eks_node_role.name
+    role       = aws_iam_role.eks_node_role.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
 resource "aws_iam_role_policy_attachment" "eks_registry_policy" {
-    role = aws_iam_role.eks_node_role.name
+    role       = aws_iam_role.eks_node_role.name
     policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
